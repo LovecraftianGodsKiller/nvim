dd09781d15181e00373393f664e822cebffa7f05
LovecraftianGodsKiller <sandfromundertal@protonmail.com>
2023-07-15 11:51:42 -0700

    Updated plugins init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,12 +1,12 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazy_path) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--file=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-cim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)

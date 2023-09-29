ae56b3f7620884d0e9370d6e2096c49d63627801
LovecraftianGodsKiller <sandfromundertal@protonmail.com>
2023-09-29 11:36:58 -0700

    updated indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,18 +1,8 @@
 return {
-<<<<<<< HEAD
     {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
         opts = {
         },
     },
-=======
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    opts = {
-      show_current_context = true,
-      show_current_context_start = true,
-    },
-  },
->>>>>>> refs/remotes/origin/main
 }

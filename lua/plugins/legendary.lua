b350d94b0cc186f9249a8bf0b601299088fb575f
LovecraftianGodsKiller <sandfromundertal@protonmail.com>
2023-10-20 13:26:25 -0700

    Updated legendary.lua

--- lua/plugins/legendary.lua
+++ lua/plugins/legendary.lua
@@ -94,30 +94,25 @@ return {
           max_timestamps = 10,
         },
       },
-      lazy_nvim = {
-    -- Automatically register keymaps that are defined on lazy.nvim plugin specs
-    -- using the `keys = {}` property.
-        auto_register = false,
-      },
-      which_key = {
-    -- Automatically add which-key tables to legendary
-    -- see ./doc/WHICH_KEY.md for more details
-        auto_register = false,
-    -- you can put which-key.nvim tables here,
-    -- or alternatively have them auto-register,
-    -- see ./doc/WHICH_KEY.md
-        mappings = {},
-        opts = {},
-    -- controls whether legendary.nvim actually binds they keymaps,
-    -- or if you want to let which-key.nvim handle the bindings.
-    -- if not passed, true by default
-        do_binding = true,
-    -- controls whether to use legendary.nvim item groups
-    -- matching your which-key.nvim groups; if false, all keymaps
-    -- are added at toplevel instead of in a group.
-        use_groups = true,
-      },
       extensions = {
+        which_key = {
+        -- Automatically add which-key tables to legendary
+        -- see ./doc/WHICH_KEY.md for more details
+            auto_register = false,
+            -- you can put which-key.nvim tables here,
+            -- or alternatively have them auto-register,
+            -- see ./doc/WHICH_KEY.md
+            mappings = {},
+            opts = {},
+            -- controls whether legendary.nvim actually binds they keymaps,
+            -- or if you want to let which-key.nvim handle the bindings.
+            -- if not passed, true by default
+            do_binding = true,
+            -- controls whether to use legendary.nvim item groups
+            -- matching your which-key.nvim groups; if false, all keymaps
+            -- are added at toplevel instead of in a group.
+            use_groups = true,
+        },
         nvim_tree = false,
         smart_splits = false,
         op_nvim = false,

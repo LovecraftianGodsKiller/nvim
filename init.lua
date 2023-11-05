0d412476d4f1491e7b26f8bfc42d5abbea3062b7
LovecraftianGodsKiller <sandfromundertal@protonmail.com>
2023-11-05 13:02:23 -0800

    updated plugins

--- init.lua
+++ init.lua
@@ -3,5 +3,7 @@ vim.opt.termguicolors = true
 
 require("config.keymaps")
 require("config.lazy")
+require("plugin-opts.cattppuccin-mocha-opts")
 require("plugin-opts.which-key-opts")
+require("plugin-opts.indent-blankline-opts")
 require("config.options")
---
+++ lua/plugin-opts/cattppuccin-mocha-opts.lua
@@ -0,0 +1,59 @@
+local mocha = require("catppuccin.palettes").get_palette "mocha"
+
+require("catppuccin").setup {
+    color_overrides = {
+        all = {
+	    rosewater = "#f5e0dc",
+	    flamingo = "#f2cdcd",
+	    pink = "#f5c2e7",
+	    mauve = "#cba6f7",
+	    red = "#f38ba8",
+	    maroon = "#eba0ac",
+	    peach = "#fab387",
+	    yellow = "#f9e2af",
+	    green = "#a6e3af",
+	    teal = "#94e2d5",
+	    sky = "#89dceb",
+	    sapphire = "#74c7ec",
+	    blue = "#89b4fe",
+	    lavender = "#b4befe",
+        text = "#cdd6f4",
+	    subtext1 = "#bac2de",
+	    subtext0 = "#a6adc8",
+	    overlay2 = "#9399b2",
+	    overlay1 = "#7f849c",
+	    overlay0 = "#6c7086",
+	    surface2 = "#585b70",
+	    surface1 = "#45475a",
+	    surface0 = "#313244",
+	    base = "#1e1e2e",
+	    mantle = "#181825",
+	    crust = "#11111b",
+        },
+    },
+    custom_highlights = function(colors)
+        return {
+            Comment = { fg = colors.flamingo },
+            TabLineSel = { bg = colors.pink },
+            CmpBorder = { fg = colors.surface2 },
+            Pmenu = { bg = colors.none },
+        }
+    end,
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+        indent_blankline = {
+            enabled = true,
+            scope_color = "", -- catppuccin color (eg. 'lavender) Default: text
+            colored_indent_levels = true,
+        },
+        which_key = true,
+    },
+}

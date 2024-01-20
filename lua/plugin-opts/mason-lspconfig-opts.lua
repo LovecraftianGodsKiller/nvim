a59e5d9ae039de4ec9f2fa9195f41b2820973d0e
LovecraftianGodsKiller <sandfromundertal@protonmail.com>
2024-01-20 15:35:51 -0800

    Removed unneeded plugins and plugin configurations

--- lua/plugin-opts/mason-lspconfig-opts.lua
+++
@@ -1,5 +0,0 @@
-require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "rust_analyzer", "marksman", "bashls", "jsonls", "taplo", "yamlls", "lemminx" },
-    automatic_isntallation = true,
-    handlers = nil,
-}

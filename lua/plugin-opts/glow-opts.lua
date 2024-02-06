32a1b3b38407c3327a32beb85a2f952f7cd20b7a
LovecraftianGodsKiller <sandfromundertal@protonmail.com>
2024-02-06 13:40:28 -0800

    added nvim-mdlinks & glow plugins & their configs

---
+++ lua/plugin-opts/glow-opts.lua
@@ -0,0 +1,4 @@
+require('glow').setup({
+    widgh_ratio = 1.0,
+    height_ratio = 1.0,
+})
---
+++ lua/plugins/nvim-mdlink.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "Nedra1998/nvim-mdlink",
+        opts = {
+            cmp = false,
+            keymap = true,
+        },
+    },
+}
